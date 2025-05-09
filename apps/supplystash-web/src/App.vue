47c48fefa2cd3b39d2bcf17ea112bd7287e7e24b
Alex Muench <ammuench@gmail.com>
2025-05-09 00:44:00 -0500

    🎨 Move to daisyui

--- apps/supplystash-web/src/App.vue
+++ apps/supplystash-web/src/App.vue
@@ -1,15 +1,11 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import { RouterLink, RouterView } from "vue-router";
+import NavBar from "@/components/NavBar.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank p-4 mt-4">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main class="h-dvh grid grid-cols-1 grid-rows-[auto_1fr]">
+    <NavBar />
+    <RouterView />
+  </main>
 </template>
--- apps/supplystash-web/vite.config.ts
+++ apps/supplystash-web/vite.config.ts
@@ -1,13 +1,17 @@
-import path from "node:path";
-import tailwindcss from "@tailwindcss/vite";
-import vue from "@vitejs/plugin-vue";
-import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
 
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import vueDevTools from "vite-plugin-vue-devtools";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
