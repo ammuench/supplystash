d3a92e76f012175aa6af2f687d263da932ad33f7
Alex Muench <ammuench@gmail.com>
2025-06-06 23:52:49 -0500

    🚧 Start setting up .env stuff

--- apps/supplystash-web/src/main.ts
+++ apps/supplystash-web/src/main.ts
@@ -7,6 +7,9 @@ import router from "./router";
 
 const app = createApp(App);
 
+// TODO: Remove this after we get all the env junk sorted out
+console.log("Supabase Check:", import.meta.env.VITE_SUPABASE_URL);
+
 app.use(createPinia());
 app.use(router);
 
