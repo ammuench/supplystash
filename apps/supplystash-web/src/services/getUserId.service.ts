aec675afbbd372383f1816d98932dd13727f1917
Alex Muench <ammuench@gmail.com>
2026-04-14 23:58:16 -0500

    ♻️ (spike/rework): Delete old code, stage base dev deps

--- apps/supplystash-web/src/services/getUserId.service.ts
+++
@@ -1,8 +0,0 @@
-import { supabase } from "@/utils/supabase";
-
-export const getUserId = async () => {
-  const { data } = await supabase.auth.getSession();
-  const userId = data.session?.user.id;
-
-  return userId;
-};
