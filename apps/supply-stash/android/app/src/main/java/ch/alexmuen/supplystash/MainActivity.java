de058c4486f1022a557846cb82181f9e6d756c8a
Alex Muench <ammuench@gmail.com>
2026-04-15 00:22:04 -0500

    📱 (spike/rework): Configure capacitor & android

---
+++ apps/supply-stash/android/app/src/main/java/ch/alexmuen/supplystash/MainActivity.java
@@ -0,0 +1,5 @@
+package ch.alexmuen.supplystash;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ apps/supply-stash/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "ch.alexmuen.supplystash",
+  appName: "Supply Stash",
+  webDir: ".output/public",
+};
+
+export default config;
